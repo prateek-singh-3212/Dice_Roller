b4d0baaa7ecd1e3a4e9f17ba72b871c257780296
Prateek Singh <prateeksingh3212@gmail.com>
2022-10-04 10:29:49 +0530

    Dice Roller UI

--- app/src/main/java/com/bitlogger/dice/MainActivity.kt
+++ app/src/main/java/com/bitlogger/dice/MainActivity.kt
@@ -3,41 +3,39 @@ package com.bitlogger.dice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.bitlogger.dice.ui.theme.DiceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DiceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            DiceRollerApp()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DiceRollerApp() {
+    DiceRoller()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    DiceTheme {
-        Greeting("Android")
+fun DiceRoller() {
+    Column(modifier = Modifier.fillMaxSize().wrapContentSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = "Dice")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Roll Dice")
+        }
     }
 }
